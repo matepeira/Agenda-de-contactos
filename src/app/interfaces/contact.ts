a8816906dbff1746dd74a78125702db4287b95bb
matepeira <mateopeirano21@gmail.com>
2025-10-22 19:06:38 -0300

    Trabajo casi termindo

--- src/app/interfaces/contact.ts
+++ src/app/interfaces/contact.ts
@@ -1,10 +1,10 @@
 export interface Contact {
+  image: string,
   id: number,
   firstName: string,
   lastName: string,
   address: string
   email: string,
-  image: string,
   number: string,
   company: string
   isFavorite: boolean
